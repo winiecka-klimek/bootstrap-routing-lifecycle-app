cb79ed7bdb23922104fa772ac7d83c2c64554748
Winiecka-Klimek <marta.winiecka@gmail.com>
2020-11-08 13:42:20 +0100

    adding page content

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,23 +1,17 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit, OnDestroy{
-
+export class AboutComponent implements OnInit, OnDestroy {
   constructor() {
     console.log('constructor()');
   }
-
   ngOnInit(): void {
     console.log('ngOnInit()');
   }
-
   ngOnDestroy(): void {
     console.log('ngOnDestroy()');
   }
-
-
 }
--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Person} from '../../model/person.model';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public people: Person[] = [];
+  constructor() {
   }
-
+  ngOnInit(): void {
+    const user: Person = {
+      firstName: 'Mateusz',
+      lastName: 'Stanczak'
+    };
+    const userTwo: Person = {
+      firstName: 'Adam',
+      lastName: 'Mickiewicz'
+    };
+    this.people.push(user, userTwo);
+  }
 }
